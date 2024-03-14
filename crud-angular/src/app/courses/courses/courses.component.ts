48bec9dd61f252c8a68f738b316a489aab76f118
maiarafaria <mf.mai@hotmail.com>
2024-03-14 11:36:52 -0300

    Creating Material Table to list courses

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Course } from '../models/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
-
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Angular-frontend' },
+  ];
+  displayedColumns = ['name', 'category'];
 }
